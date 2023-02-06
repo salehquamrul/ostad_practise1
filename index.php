6526f29c4842ee8554839d18951c448bdea64252
salehquamrul <saleh.quamrul@gmail.com>
2023-02-07 00:21:08 +0600

    same

--- index.php
+++ index.php
@@ -4,6 +4,8 @@ $tuitionFee = 100000;
 
 $commission = ($tuitionFee >= 20000) ? ($tuitionFee * .25) : (($tuitionFee >= 10000 && $tuitionFee < 20000) ? ($tuitionFee * .20) : (($tuitionFee >= 7000 && $tuitionFee < 10000) ? ($tuitionFee * .15) : ("Invalid Data.")));
 
-echo "The commission is {$commission}."
+echo "\n";
 
-?>
+$commission = ($tuitionFee<7000)?("invalid data"):(($tuitionFee>=7000 && $tuitionFee<10000)?($tuitionFee*.15):(($tuitionFee>=10000 && $tuitionFee<20000 )?($tuitionFee * .20):(($tuitionFee>=20000)?($tuitionFee*.25):())));
+
+echo "The commission is {$commission}.";
